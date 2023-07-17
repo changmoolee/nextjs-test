85baa281d3e50d381f534202886dfb96e4c26980
Lee Changmoo <lcm8543@gmail.com>
2023-07-17 22:40:22 +0900

    서버컴포넌트 - node api 사용 가능

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import os from "os";
+import { useState } from "react";
+
 export default function Home() {
+  console.log("안녕");
+  console.log(os.hostname());
+
+  const [name, setName] = useState("");
+
   return <h1>홈페이지다!!!!!</h1>;
 }
