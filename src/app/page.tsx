35f54cfe5f652045a360b76b7a6b8a9e5eaa25d3
Lee Changmoo <lcm8543@gmail.com>
2023-07-18 23:25:48 +0900

    Fix: 서버 컴포넌트에서 클라이언트 컴포넌트 사용하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-"use client";
-
 import Counter from "@/components/Counter";
 import os from "os";
-import { useState } from "react";
 
 export default function Home() {
   console.log("안녕");
   console.log(os.hostname());
-
-  const [name, setName] = useState("");
 
   return (
     <>
