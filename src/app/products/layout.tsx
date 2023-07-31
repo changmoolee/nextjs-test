f047f752d4804f830a437ed26f403c91629792bf
Lee Changmoo <lcm8543@gmail.com>
2023-07-31 22:44:32 +0900

    nextjs에서 제공해주는 font 사용하기

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import styles from "./layout.module.css";
 import { Metadata } from "next";
+import { Nanum_Gothic, Open_Sans } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "옷 판매 사이트 | 옷 전체 메뉴",
@@ -9,6 +10,12 @@ export const metadata: Metadata = {
     icon: "/favicon.ico",
   },
 };
+const sans = Open_Sans({ subsets: ["latin"] });
+
+const gothic = Nanum_Gothic({
+  weight: "700",
+  subsets: ["latin"],
+});
 
 interface IProductLayoutProps {
   children: React.ReactNode;
@@ -18,8 +25,12 @@ export default function ProductLayout({ children }: IProductLayoutProps) {
   return (
     <div>
       <nav className={styles.nav}>
-        <Link href={"products/women"}>여성옷</Link>
-        <Link href={"products/men"}>남성옷</Link>
+        <Link href={"products/women"} className={sans.className}>
+          여성옷
+        </Link>
+        <Link href={"products/men"} className={gothic.className}>
+          남성옷
+        </Link>
       </nav>
       {/* 리액트 서스펜스로 감싸지는 부분 (로딩) */}
       {children}
